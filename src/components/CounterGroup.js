6c439db08dd029c68bc0efaa9e951524bdc73b33
Theo Lo <theo.lo@oocl.com>
2020-12-07 17:05:00 +0800

    [Counter][Theo] feat: generated multiple counters according to props with static number

---
+++ src/components/CounterGroup.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Counter from "./Counter"
+
+class CounterGroup extends Component {
+    initArraySize = (size) => {
+        return Array.from(Array(size).keys());
+    };
+
+    render() {
+        const size = this.props.size;
+        const initArraySize = this.initArraySize(size);
+
+        return (
+            <div>
+                {
+                    initArraySize.map((value) =>
+                        <Counter key={value} />
+                    )
+                }
+            </div>
+        );
+    }
+}
+
+export default CounterGroup;
